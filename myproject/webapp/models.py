d9c028fe10f68591b363b28857efe41102110b5e
ceaganvs <ceaganvs@gmail.com>
2025-12-09 23:30:01 +0200

    Add docstrings to models.py

--- myproject/webapp/models.py
+++ myproject/webapp/models.py
@@ -1,7 +1,28 @@
+"""
+Database models for the Red-Haired Pirates crew management system.
+
+This module contains the CrewMember model for storing information
+about individuals who want to join Shanks' pirate crew.
+"""
+
 from django.db import models
 
 
 class CrewMember(models.Model):
+    """
+    Model representing a crew member application for the Red Hair Pirates.
+    
+    Attributes:
+        name (str): Full name of the applicant
+        email (str): Unique email address for contact
+        age (int): Age of the applicant
+        hometown (str): Place of origin
+        role (str): Preferred role on the ship (fighter, navigator, etc.)
+        skills (str): Description of the applicant's abilities
+        reason (str): Why they want to join the crew
+        joined_date (datetime): Timestamp when the application was submitted
+    """
+    
     ROLE_CHOICES = [
         ('fighter', 'Fighter'),
         ('navigator', 'Navigator'),
@@ -22,6 +43,7 @@ class CrewMember(models.Model):
     joined_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
+        """Return string representation of the crew member."""
         return f"{self.name} - {self.role}"
     
     class Meta:
